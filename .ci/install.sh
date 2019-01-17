1f20bdf699b63c423c584c80f15ebdc9379601a3
Diego Ferigo <diego.ferigo@iit.it>
2019-01-17 11:57:49 +0100

    Extra job that use clang-cl for compiling on Windows

--- .ci/install.sh
+++ .ci/install.sh
@@ -15,10 +15,26 @@ if [ "$TRAVIS_CMAKE_GENERATOR" = "Visual Studio 15 2017" ] ; then
     cmake --build . --config $TRAVIS_BUILD_TYPE
     cmake --build . --config $TRAVIS_BUILD_TYPE --target INSTALL
 else
-    cmake -G"$TRAVIS_CMAKE_GENERATOR" \
-          -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
-          -DCMAKE_INSTALL_PREFIX="$DEPS_INSTALL_PREFIX" \
-          ..
-    cmake --build .
-    cmake --build . --target install
+    if [ "$TRAVIS_OS_NAME" = "windows" ] ; then
+        # TODO: watch https://gitlab.kitware.com/cmake/cmake/issues/18745
+        cmake -G"$TRAVIS_CMAKE_GENERATOR" \
+              -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
+              -DCMAKE_INSTALL_PREFIX="$DEPS_INSTALL_PREFIX" \
+              -DCMAKE_C_COMPILER=clang-cl \
+              -DCMAKE_CXX_COMPILER=clang-cl \
+              -DCMAKE_LINKER=lld-link \
+              -DCMAKE_GENERATOR_RC=llvm-rc \
+              -DCMAKE_RC_COMPILER=llvm-rc \
+              -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld-link" \
+              ..
+        cmake --build .
+        cmake --build . --target install
+    else
+        cmake -G"$TRAVIS_CMAKE_GENERATOR" \
+              -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
+              -DCMAKE_INSTALL_PREFIX="$DEPS_INSTALL_PREFIX" \
+              ..
+              cmake --build .
+              cmake --build . --target install
+    fi
 fi
